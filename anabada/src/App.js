30964336b18f59afafbc24d0c0a3fb63f35a3138
ljy6712 <a01045386712@gmail.com>
2023-11-14 12:59:13 +0900

    채팅 중간저장

--- anabada/src/App.js
+++ anabada/src/App.js
@@ -6,28 +6,29 @@ import List from "./Main/List";
 import Signin from "./Signupin/Signin";
 import Signup from "./Signupin/Signup";
 import CategoryList from "./Category/CategoryList";
+import VerticalTabs from "./Chatting/ChatList";
 import ChatPage from "./Chatting/ChatPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-            
-              <PrimarySearchAppBar />
-              <CategoryList />
-            </>
-          }
-        />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <>
+                            <PrimarySearchAppBar />
+                            <CategoryList />
+                        </>
+                    }
+                />
+                <Route path="/chat" element={<VerticalTabs />} />
+                {/* <Route path="/chat" element={<ChatPage />} /> */}
+                <Route path="/signin" element={<Signin />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- anabada/src/Chatting/ChatPage.js
+++ anabada/src/Chatting/ChatPage.js
@@ -1,12 +1,12 @@
 import Channel from "./channel";
-import { useRouter } from 'react-router-dom';
+// import { useRouter } from 'react-router-dom';
 
 const ChatPage = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  const { currentUser } = localStorage.getItem("currentUser");
-
-  return <>{currentUser && <Channel id={id} />}</>;
+    // const router = useRouter();
+    const id = "user2@gmail.com";
+    const currentUser = localStorage.getItem("currentUser");
+    console.log(currentUser);
+    return <>{currentUser && <Channel id={id} />}</>;
 };
 
 export default ChatPage;
