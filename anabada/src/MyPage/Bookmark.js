b231d3ebde5dfe421c473f177873c4b78554aa00
ljy6712 <a01045386712@gmail.com>
2023-11-22 00:14:16 +0900

    리코일 되는지 확인

--- anabada/src/MyPage/Bookmark.js
+++ anabada/src/MyPage/Bookmark.js
@@ -2,9 +2,10 @@
 
 import React from "react";
 import BookmarkItem from "./BookmarkItem";
-import UserData from "../Data/UsersData.json";
 import { List, Typography } from "@mui/material";
 import { Box, Paper } from "@mui/material";
+import { useRecoilValue } from "recoil";
+import { usersState } from "../ProductState/UsersState";
 
 /*
  * json 구조
@@ -16,32 +17,38 @@ import { Box, Paper } from "@mui/material";
  */
 
 const Bookmark = ({ userId }) => {
-  const [{ bookMarkData }] = UserData.filter((user) => user.id === userId);
-  console.log(bookMarkData);
-  return (
-    <>
-      <Paper
-        sx={{
-          height: "500px",
-          width: "300px",
-          backgroundColor: "white",
-          borderRadius: 5,
-          border: 0,
-          padding: "10px",
-          elevation: 3,
-        }}
-      >
-        <Typography variant="h4" align="center" margin={2}>
-          북마크
-        </Typography>
-        <List sx={{ bgColor: "blue" }}>
-          {bookMarkData.map(({ ProductId }, idx) => (
-            <BookmarkItem productId={ProductId} key={`${userId}-${idx}`} />
-          ))}
-        </List>
-      </Paper>
-    </>
-  );
+    const UserData = useRecoilValue(usersState);
+    console.log(UserData);
+    console.log(userId);
+    const [{ bookMarkData }] = UserData.filter((user) => user.id === userId);
+    console.log(bookMarkData);
+    return (
+        <>
+            <Paper
+                sx={{
+                    height: "500px",
+                    width: "300px",
+                    backgroundColor: "white",
+                    borderRadius: 5,
+                    border: 0,
+                    padding: "10px",
+                    elevation: 3,
+                }}
+            >
+                <Typography variant="h4" align="center" margin={2}>
+                    북마크
+                </Typography>
+                <List sx={{ bgColor: "blue" }}>
+                    {bookMarkData.map(({ ProductId }, idx) => (
+                        <BookmarkItem
+                            productId={ProductId}
+                            key={`${userId}-${idx}`}
+                        />
+                    ))}
+                </List>
+            </Paper>
+        </>
+    );
 };
 
 export default Bookmark;
