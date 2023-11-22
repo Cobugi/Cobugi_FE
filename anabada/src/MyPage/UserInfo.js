66bf095e11d8a822975db6f7c8273c159d8db609
YoonhoJo99 <we424239@gmail.com>
2023-11-22 19:54:05 +0900

    유저인포 추가

--- anabada/src/MyPage/UserInfo.js
+++ anabada/src/MyPage/UserInfo.js
@@ -7,21 +7,43 @@ import { seekingProductsState } from "../ProductState/SeekingProductsState";
 import { useRecoilValue } from "recoil";
 
 const UserInfo = () => {
+    const curEmail = localStorage.getItem('currentUser');
     const curName = localStorage.getItem('currentUser').split('@')[0];
 
     const lend = useRecoilValue(lendingProductsState);
     const seek = useRecoilValue(seekingProductsState);
 
+    //내가 등록한 물건
+    //lend와 seek에서 registeredUserId가 나의 id인 경우
+    const myRegister_lend = lend.filter((product => product.registeredUserId === curEmail));
+    const myRegister_seek = seek.filter((product => product.registeredUserId === curEmail));
+    const myRegister = [...myRegister_lend, ...myRegister_seek];
+
     //내가 빌린 물건은 2가지 경우의 수가 있다.
-    //1. lend 각 원소에 reservedUsersInfo(배열)을 체크해서 각 원소에 "reservedUser"에 내 id가 있을 경우..
+    // 1. lend 각 원소에 reservedUsersInfo(배열) 각 원소에 "reservedUser"배열에 내 id가 있을 경우..
+    const mySeek_lend = lend.filter(product =>
+        product.reservedUsersInfo.some(reservedUser =>
+          reservedUser.reservedUser === curEmail
+        )
+    );
     //2. seek 각 원소에 registeredUserId가 내 id이고 state가 1인 경우
-                                            
+    const mySeek_seek = seek.filter((product => product.registeredUserId === curEmail));  
+    //3. 내가 빌린 물건 최종
+    const mySeek = [...mySeek_lend, ...mySeek_seek];
+  
 
     //내가 빌려준 물건은 2가지 경우의 수가 있다.
     //1. lend 각 원소에 registeredUserId가 내 id이고 state가 1인 경우
+    const myLend_lend = lend.filter((product => product.registeredUserId === curEmail && product.state === '1'));
     //2. seek 각 원소에 lendingUserId가 나인 경우
-
+    const myLend_seek = seek.filter((product => product.lendingUserId === curEmail));
+    //3. 내가 빌려준 물건 최종
+    const myLend = [...myLend_lend, ...myLend_seek]
     
+    console.log(myRegister)
+    console.log(mySeek)
+    console.log(myLend)
+
     // product에서 
 
     // const mySeek
@@ -44,7 +66,7 @@ const UserInfo = () => {
                             등록 물건
                         </Typography>
                         <Typography sx={{ width: '50%',height: '50%', backgroundColor:"white",textAlign: 'center',display: 'flex',alignItems: 'center',justifyContent: 'center',position: 'relative',top: '50%',left: '50%',transform: 'translate(-50%, -150%)'}}>
-                            건수 : 3
+                            건수 : {myRegister.length}
                         </Typography>
                     </Box>
                     <Box sx={{border:1, height:"40px", width:"150px", margin:"auto", marginTop:2, marginBottom:2}}>
@@ -52,7 +74,7 @@ const UserInfo = () => {
                             빌린 물건
                         </Typography>
                         <Typography sx={{ width: '50%',height: '50%', backgroundColor:"white",textAlign: 'center',display: 'flex',alignItems: 'center',justifyContent: 'center',position: 'relative',top: '50%',left: '50%',transform: 'translate(-50%, -150%)'}}>
-                            건수 : 3
+                            건수 : {mySeek.length}
                         </Typography>
                     </Box>
                     <Box sx={{border:1, height:"40px", width:"150px", margin:"auto"}}>
@@ -60,7 +82,7 @@ const UserInfo = () => {
                             빌려준 물건
                         </Typography>
                         <Typography sx={{ width: '50%',height: '50%', backgroundColor:"white",textAlign: 'center',display: 'flex',alignItems: 'center',justifyContent: 'center',position: 'relative',top: '50%',left: '50%',transform: 'translate(-50%, -150%)'}}>
-                            건수 : 3
+                            건수 : {myLend.length}
                         </Typography>
                     </Box>
                 </Box>
